49e61f296a96f47d69a51f65dbf44d111c481184
FengHua <573009114@qq.com>
2025-07-15 15:59:54 +0800

    Delete wechat-cp/wechat-cp directory

--- wechat-cp/wechat-cp/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# 微信公众号配置（请替换为你的真实信息）
-WECHAT_APPID = os.getenv("WECHAT_APPID", "your_appid_here")
-WECHAT_APPSECRET = os.getenv("WECHAT_APPSECRET", "your_appsecret_here")
-
-# 可扩展更多配置，如token、encoding_aes_key等 
